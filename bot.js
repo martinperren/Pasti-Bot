6a0f77aece3f7a73f88892162eb0184e38c26f9b
Martin Perren <martinperren@gmail.com>
2020-04-30 20:14:59 -0300

    test

--- bot.js
+++ bot.js
@@ -2,32 +2,6 @@
 var Twit = require('twit');
 var fs = require('fs');
 
-// Set Twitter API keys
-var TWITTER_CONSUMER_KEY = '';
-var TWITTER_CONSUMER_SECRET = '';
-var TWITTER_ACCESS_TOKEN = '';
-var TWITTER_ACCESS_TOKEN_SECRET = '';
-
-// Database file
-var DB_FILE = "bot_db.txt";
-
-// Set interval time. Try to use a not so small interval to avoid Twitter to lock your account.
-var INTERVAL = 3*60*60*1000; // 3 hours
-
-// Set Twitter search phrase. You can use hash tags or simples text. Hash tags works better. Separate with OR or AND.
-var TWITTER_SEARCH_PHRASE = '#AGoodHashTag OR #AnotherOne OR #MyGreatSearch';
-
-// Set max number of tweets to get on the search results each time
-var TWITTER_SEARCH_MAX_RESULTS = 20;
-
-// Set tweets to reply
-var TWEETS_TO_REPLY = [
-	"Write something to reply.",
-	"Bot will randomly pick one of the tweets to send.",
-	"Try to be very natural and human like on the replies.",
-	"Use the main Twitter handle you want to promote in all options. @MyHandle",
-	"Create 5 to 10 replies. The more options the more natural bot will look."
-];
 
 // Init Twit lib
 const T = new Twit({
@@ -39,7 +13,7 @@ const T = new Twit({
 });
 
 // Retwittear Tweets que contenga el Hashtag #css
-const stream = T.stream('statuses/filter', { track: '#css' });
+const stream = T.stream('statuses/filter', { track: '@Protomolecula' });
  
 // Registro errores en las solicitudes
 function responseCallback(err, data, response) {
