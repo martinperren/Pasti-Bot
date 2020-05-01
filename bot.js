c1e64d356f7bad96e01dfe28326299b7b61515a5
Martin Perren <martinperren@gmail.com>
2020-04-30 21:03:24 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,6 @@ var stream = T.stream('statuses/filter', { locations: sanFrancisco })
  
 stream.on('tweet', function (tweet) {
   if(tweet.user.location!=null)
-  console.log("Location: "+tweet.user.location!=null+"Fullname: "+tweet.place.fullname)
+  console.log("Location: "+tweet.user.location+"Fullname: "+tweet.place.fullname)
 })
 
