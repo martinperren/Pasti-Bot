704cc83e833981010fe35c2526e0175ba09523fc
Martín Sergio Perren <mperren@santafe.gov.ar>
2021-02-23 04:44:38 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ rule.tz = 'America/Argentina/Buenos_Aires';
 
 let jsonData = require('./localidades.json');
 
-var localidad = jsonData.localidades[getRandomArbitrary(1,3500)];
+var localidad = jsonData.localidades[0];
 console.log(localidad);
 const job = schedule.scheduleJob(rule, function(){
 
