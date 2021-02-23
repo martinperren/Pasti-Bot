e3f9cf8c6e934104b5dde375bcb15073cd87a4d6
Martín Sergio Perren <mperren@santafe.gov.ar>
2021-02-23 04:49:01 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,53 +1,42 @@
 // Create object reference to libs
-var Twit = require('twit');
-var fs = require('fs');
-const schedule = require('node-schedule');
-
+var Twit = require("twit");
+var fs = require("fs");
+const schedule = require("node-schedule");
 
 function getRandomArbitrary(min, max) {
-	return Math.random() * (max - min) + min;
-  }
+  return Math.random() * (max - min) + min;
+}
 
 console.log("Bot iniciado");
 // Init Twit lib
 
-
-
 const T = new Twit({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token: process.env.TWITTER_ACCESS_TOKEN,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-
 const rule = new schedule.RecurrenceRule();
-const ybp = require('year-progress-bar');
+const ybp = require("year-progress-bar");
 rule.hour = 23;
 
+rule.tz = "America/Argentina/Buenos_Aires";
 
-rule.tz = 'America/Argentina/Buenos_Aires';
-
-let jsonData = require('./localidades.json');
+let jsonData = require("./localidades.json");
 
 var localidad = jsonData.localidades[0].municipio.nombre;
 
 console.log(localidad);
-const job = schedule.scheduleJob(rule, function(){
+const job = schedule.scheduleJob(rule, function () {
+  var numero = getRandomArbitrary(0, 3300);
+  console.log(numero);
 
-
-	
-
-
-
-
-	T.post('statuses/update', { status: "TOMA LA PASTILLA"}, function(err, data, response) {
-
-
-		
-		console.log(data)
-	  })
+  T.post(
+    "statuses/update",
+    { status: "TOMA LA PASTILLA" },
+    function (err, data, response) {
+      console.log(data);
+    }
+  );
 });
-
-
-
